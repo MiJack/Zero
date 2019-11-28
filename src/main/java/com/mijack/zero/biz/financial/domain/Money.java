01d6a1e53245dc47069de27bbde8f2090a772909
Mi&Jack <mijackstudio@gmail.com>
2019-11-29 01:07:20 +0800

    update copyright

--- src/main/java/com/mijack/zero/biz/financial/domain/Money.java
+++ src/main/java/com/mijack/zero/biz/financial/domain/Money.java
@@ -1,3 +1,19 @@
+/*
+ *     Copyright 2019 Mi&Jack
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package com.mijack.zero.biz.financial.domain;
 
 import lombok.Data;
@@ -5,14 +21,14 @@ import lombok.Data;
 /**
  * 用于表示货币单位
  *
- * @author yuanyujie
+ * @author Mi&amp;Jack
  */
 @Data
 public class Money {
     /**
      * 货币单位
      */
-    private CurrencyUnit currencyUnit;
+    private Currency currency;
     /**
      * 具体数值
      */
